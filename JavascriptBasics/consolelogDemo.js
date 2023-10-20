568f9bd61a6baa146f8f4cfa56e567aaa287ebc6
Tejas Patel <tejas7016652830@gmail.com>
2023-10-20 19:22:48 +0530

    file upadated

---
+++ JavascriptBasics/consolelogDemo.js
@@ -0,0 +1,22 @@
+// input-----> console.log() ----> output
+// example console.log(<wthat you have to print write here>);
+
+console.log("Peter"); // print peter
+console.log(20); // print 20
+console.log(1 + 2); // print 3
+
+//we can create variavble and pass to the console.log
+
+var name = "peter";
+console.log(name);
+
+let marks = 99;
+console.log(marks);
+
+const age = 21;
+
+//we can also pass multiple values in console.log
+
+console.log(age, name, marks);
+
+console.log(1 + 2, 3 - 1, 2 * 3);
