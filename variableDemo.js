aee38618637eea1ac21249e7a6180294e5c4ca5f
Tejas Patel <tejas7016652830@gmail.com>
2023-10-20 10:27:28 +0530

    added variableDemo File

---
+++ variableDemo.js
@@ -0,0 +1,27 @@
+//three way to create variables in javascript
+
+// 1. Using Var Keyword
+// Syntax -- var <variable Name> = <values>
+// Example -- var marks = 33;
+var name = "peter";
+var score = "4"; // this is the firs way to create js variable
+
+// 2. Using Let Keyword
+// Syntax -- let <variable Name> = <values>
+// Example -- let marks = 33;
+let age = 21;
+let flag = false;
+
+// 3. Using Const Keyword
+// Syntax -- const <variable Name> = <values>
+// Example -- const marks = 33;
+const nameIs = "peter Parker";
+//-------------------------------------------------------
+//semicolon -- always put after the javascript code
+//--------------------------------------------------------
+//---naming rules for variables creation---
+//--small alphabets, capital alphabets, digits, underscore(_) and dollar($)
+//--cannot have space or any other special char.
+//--give meaningful name to variables
+//--cannot start with digit but it can have digits in between or atlast e.g. let 0James = 10; --> this is wrong Syntax
+//--cannot use keyword as variables
